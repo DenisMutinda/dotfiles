891d5e742be7ae10d0e2c3a6d0b93f0207cd8cd1
DenisMutinda <dennismutinda007@gmail.com>
2020-09-27 14:02:57 +0200

    Done with rofi

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -280,12 +280,7 @@ globalkeys = gears.table.join(
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
               {description = "jump to urgent client", group = "client"}),
     awful.key({ modkey,           }, "Tab",
-        function ()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end,
+        function () awful.util.spawn("rofi -show window") end,
         {description = "go back", group = "client"}),
 
     -- Standard program
@@ -326,7 +321,7 @@ globalkeys = gears.table.join(
               {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
+    awful.key({ modkey },            "r",     function () awful.util.spawn("rofi -show drun") end,
               {description = "run prompt", group = "launcher"}),
 
     awful.key({ modkey }, "x",
